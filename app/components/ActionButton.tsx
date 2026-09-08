d87e6fb8335c5d9f008023674cf14975125c11a3
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-09-08 18:35:19 +0200

    feat(frontend): make homepage fully responsive

--- app/components/ActionButton.tsx
+++ app/components/ActionButton.tsx
@@ -15,7 +15,19 @@ export default function ActionButton({
     <button
       type="submit"
       className="
-        mt-8 w-[50%] rounded-lg bg-blue-analyze py-4 text-2xl font-semibold text-white transition-colors hover:bg-blue-analyze/90 focus:outline-none focus:ring-2 focus:ring-blue-analyze focus:ring-offset-2 dark:bg-blue-analyze/80 dark:hover:bg-blue-analyze/70 dark:focus:ring-blue-analyze/80
+        mt-8 w-[50%] rounded-lg bg-blue-analyze 
+        py-4 text-2xl font-semibold text-white 
+        px-4
+        transition-colors hover:bg-blue-analyze/90 
+
+        focus:outline-none 
+        focus:ring-2 
+        focus:ring-blue-analyze 
+        focus:ring-offset-2
+
+        dark:bg-blue-analyze/80 
+        dark:hover:bg-blue-analyze/70 
+        dark:focus:ring-blue-analyze/80
       "
       onClick={onClick}
       disabled={disabled}
